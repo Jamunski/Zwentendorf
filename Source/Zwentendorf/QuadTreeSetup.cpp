f65f4b98412ab788dad1ac86bab5a72a1037a2cf
Viktoriya Voinova <viktoriya@iversoft.ca>
2018-02-25 19:35:22 -0500

    Does not compile, just saving WIP on github :P

--- Source/Zwentendorf/QuadTreeSetup.cpp
+++ Source/Zwentendorf/QuadTreeSetup.cpp
@@ -1,13 +1,88 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "QuadTreeSetup.h"
+#include "AABB.h"
 
-void UQuadTreeSetup::Setup(FRandomStream randomStream)
+UAABB* UQuadTreeSetup::NewQuadCell(FVector center, FVector half)
 {
-	MapDimensions *= FVector(100.0f, 100.0f, 1.0f);
+	UAABB* createdAABB = new UAABB(center, half);
 
+	AABBs.Add(createdAABB);
+
+	return createdAABB;
+}
+
+void UQuadTreeSetup::Setup(FRandomStream randomStream, FVector position)
+{
+	Settings.RandomStream = randomStream;
+
+	MapDimensions *= FVector(100.0f, 100.0f, 1.0f);
+	WorldPosition = position;
+
+	NewQuadCell(WorldPosition + MapDimensions / 2, MapDimensions / 2);
+
+	int sectorBoundaries = Settings.RoomWallBorder * 2 + Settings.SliceRoomMinSize;
+	FVector dimensionsOver100 = Settings.Dimensions / 100.0f;
+	int roomMaxAmount = FMath::Ceil( (dimensionsOver100.X / sectorBoundaries) * (dimensionsOver100.Y / sectorBoundaries) * 0.75f);
+	
+	int sliceTries = 0;
+	bool sliceBreak;
+	for (int i = 0; i < roomMaxAmount; i++)
+	{
+		sliceBreak = true;
+		
+		if (AABBs.Num() < roomMaxAmount)
+			break;
+		
+		auto currentAABB = AABBs[i];
+		sliceTries = 0;
+		while(sliceTries < SliceMaxTries)
+		{
+			for (int j = 0; j < SliceMaxTries; j++)
+			{
+				sliceTries = j;
+				bool canDropQuad = randomStream.RandRange(1, 100) <= Settings.ChanceToDropQuads;
+
+				if (i >= 4 && !canDropQuad)
+					break;
+
+				GenerateQuads(currentAABB);
+			}
+
+			sliceBreak = false;
+		}
+	}
+}
+
+void UQuadTreeSetup::GenerateQuads(UAABB* aabb)
+{
+	FVector sliceResult = RandomSlice(aabb);
+
+	//VV-TODO: finish this function
 
 }
 
+FVector UQuadTreeSetup::RandomSlice(UAABB* aabb)
+{
+	FBoundaries boundaries = aabb->GetBoundaries();
+
+	int horizontalSlice = Settings.RandomStream.RandRange(FMath::Floor(boundaries.Left)/Settings.TileSize,
+		FMath::Floor(boundaries.Right) / Settings.TileSize);
+	int verticalSlice = Settings.RandomStream.RandRange(FMath::Floor(boundaries.Left) / Settings.TileSize,
+		FMath::Floor(boundaries.Right) / Settings.TileSize);
+
+	return FVector(horizontalSlice * Settings.TileSize, verticalSlice * Settings.TileSize);
+}
+
+
+UQuadTreeSetup::UQuadTreeSetup(FQuadTreeInfo info)
+{
+	Settings = info;
+}
+
+UQuadTreeSetup::UQuadTreeSetup()
+{
+	Settings = FQuadTreeInfo();
+}
 
 
--- Source/Zwentendorf/QuadTreeSetup.h
+++ Source/Zwentendorf/QuadTreeSetup.h
@@ -9,15 +9,62 @@
 /**
  * 
  */
+class UAABB;
+
+USTRUCT()
+struct ZWENTENDORF_API FQuadTreeInfo
+{
+	GENERATED_USTRUCT_BODY()
+
+	FVector Dimensions;
+	int SliceRoomMinSize;
+	int TileSize;
+	float ChanceToDropQuads;
+	int RoomWallBorder;
+	FRandomStream RandomStream;
+
+	FQuadTreeInfo(FVector dimensions, int sliceRoomMinSize, int tileSize, float chanceToDropQuad, int roomWallBorder)
+	{
+		Dimensions = dimensions;
+		SliceRoomMinSize = sliceRoomMinSize;
+		TileSize = tileSize;
+		ChanceToDropQuads = chanceToDropQuad;
+		RoomWallBorder = roomWallBorder;
+	};
+
+	FQuadTreeInfo()
+	{
+		Dimensions = FVector::ZeroVector;
+		SliceRoomMinSize = 0;
+		TileSize = 0;
+		ChanceToDropQuads = 0.0f;
+		RoomWallBorder = 0;
+	};
+};
+
 UCLASS()
 class ZWENTENDORF_API UQuadTreeSetup : public UObject
 {
 	GENERATED_BODY()
 	
 public:
-
-	void Setup(FRandomStream randomStream);
+	UQuadTreeSetup(FQuadTreeInfo info);
+	UQuadTreeSetup();
+	void Setup(FRandomStream randomStream, FVector position);
 
 private:
 	FVector MapDimensions;
+	FVector WorldPosition;
+	UAABB* NewQuadCell(FVector center, FVector half);
+	TArray<UAABB*> AABBs;
+	FQuadTreeInfo Settings;
+
+	int SliceMaxTries = 25;
+
+	bool Slices;
+
+	void GenerateQuads(UAABB* aabb);
+	
+	FVector RandomSlice(UAABB* aabb);
+
 };
